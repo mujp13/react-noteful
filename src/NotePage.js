43101d14810acda70e09df87358e1b2fd625334f
Kevin Park <keunsuk413@gmail.com>
2020-02-25 01:59:32 -0600

    added del button in content

--- src/NotePage.js
+++ src/NotePage.js
@@ -1,27 +1,71 @@
 import React, { Component } from 'react';
-import NotefulContext from './NotefulContext'
+import NotefulContext from './NotefulContext';
 import './NotePage.css';
+
+function deleteNoteRequest(noteId, cb) {
+  fetch(`http://localhost:9090/notes/${noteId}`, {
+    method: 'DELETE',
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+    .then(res => {
+      if (!res.ok) {
+        // get the error message from the response,
+        return res.json().then(error => {
+          // then throw it
+          throw error;
+        });
+      }
+      return res.json();
+    })
+    .then(data => {
+      // call the callback when the request is successful
+      // this is where the App component can remove it from state
+      console.log({ data });
+      cb(noteId);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
 
 export default class NotePage extends Component {
   static contextType = NotefulContext;
 
   render() {
-    const note = this.context.notes.find(p =>
-      p.id === this.props.match.params.noteId
-    )
+    const note = this.context.notes.find(p => p.id === this.props.match.params.noteId);
+
+    const today = new Date();
+    const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    const time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+    const dateTime = date + ' ' + time;
 
     return (
-      <div className='note-content'>
-        <div className='note-header'>
-          <h2>{note.name}</h2>
-          {note.modified ? <p>Date modified on {note.modified}</p>
-                         : <p>(added by user)</p>
-          }
-        </div>
-        <div className='note-desc'>
-          <p>{note.content}</p>
-        </div>
-      </div>
-    )
+      <NotefulContext.Consumer>
+        {context => (
+          <div className="note-content">
+            <div className="note-header">
+              <h2>{note.name}</h2>
+                <div className="sub-content">
+                  {note.modified ? <p>Date modified on {note.modified}</p> : <p>Date modified on {dateTime}</p>}
+                  <button
+                    className="delete-button"
+                    onClick={() => {
+                      deleteNoteRequest(note.id, context.deleteNote);
+                      this.props.history.push('/');
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            <div className="note-desc">
+              <p>{note.content}</p>
+            </div>
+          </div>
+        )}
+      </NotefulContext.Consumer>
+    );
   }
 }
